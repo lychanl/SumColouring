08cbd482e154d326578555d59ba9c884bf360d8d
lychanl <lycha_@poczta.onet.pl>
2019-05-17 00:39:14 +0200

    Implementation for solver using Greedy Independent Set algorithm

---
+++ src/GISSolver.h
@@ -0,0 +1,19 @@
+#include "Solver.h"
+
+namespace SumColouring
+{
+    class GISSolver : public Solver
+    {
+        typedef std::set<int> verticeEdges;
+        const int NO_COLOUR = -1;
+    public:
+        virtual std::vector<int> findColouring(const Graph&);
+
+    private:
+        void findColouring(
+            std::vector<int>& colouring,
+            std::set<int>& unassignedVertices,
+            std::vector<verticeEdges>& edges,
+            int nextColour);
+    };
+}
