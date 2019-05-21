0f743fa17258a102bd1a4358dbbde7c199140c6f
lychanl <lycha_@poczta.onet.pl>
2019-05-22 01:48:13 +0200

    Solver factory

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,9 +1,26 @@
 #include "Solver.h"
+#include "BruteForceSolver.h"
+#include "GISSolver.h"
+#include "LinearProgrammingSolver.h"
+#include "MaxSATSolver.h"
 
 #include <vector>
 #include <algorithm>
 
 using namespace SumColouring;
+
+std::unique_ptr<Solver> Solver::create(const std::string& name)
+{
+	if (name == "brute-force")
+		return std::unique_ptr<Solver>(new BruteForceSolver);
+	if (name == "gis")
+		return std::unique_ptr<Solver>(new GISSolver);
+	if (name == "linear")
+		return std::unique_ptr<Solver>(new LinearProgrammingSolver);
+	if (name == "maxsat")
+		return std::unique_ptr<Solver>(new MaxSATSolver);
+	return std::unique_ptr<Solver>(nullptr);
+}
 
 //Max graph degree + 1
 int Solver::getMaxColour(const Graph& g)
