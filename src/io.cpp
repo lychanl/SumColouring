74ebc07dbeee03435a31931a4d7c278c93c012ce
Michał Chilczuk <michal.chilczuk@prognosis.pl>
2019-05-22 01:25:53 +0200

    Assume input is always valid and use streams directly

--- src/io.cpp
+++ src/io.cpp
@@ -1,57 +1,21 @@
 #include <sstream>
 #include "io.h"
-#include <memory>
 
 using namespace SumColouring;
 
-std::unique_ptr<std::pair<int, int>> parseLine(const std::string &line) {
-    std::stringstream ss(line);
-
-    std::string temp;
-    ss >> temp;
-    int firstColumn = strtol(temp.c_str(), nullptr, 10);
-    ss >> temp;
-    int secondColumn = strtol(temp.c_str(), nullptr, 10);
-
-    return std::unique_ptr<std::pair<int, int>>(new std::pair<int, int>{firstColumn, secondColumn});
-}
-
-std::unique_ptr<std::pair<int, int>> readNextLine(std::istream &in) {
-    std::string linebuffer;
-    while (in && getline(in, linebuffer)) {
-        if (linebuffer.length() == 0) {
-            continue;
-        }
-
-        return parseLine(linebuffer);
-    }
-
-    return nullptr;
-}
-
 Graph SumColouring::readGraph(std::istream &in) {
+    int numberOfVertices, numberOFEdges;
+    in >> numberOfVertices >> numberOFEdges;
 
+    int v1, v2;
     std::set<Graph::Edge> edges;
-
-    auto line = readNextLine(in);
-    int numberOfVertices = line->first;
-    int numberOFEdges = line->second;
-
-    int readEdges = 0;
-    while(readEdges < numberOFEdges) {
-        line = readNextLine(in);
-
-        edges.insert(*line);
-
-        ++readEdges;
+    for(int readEdges = 0; readEdges < numberOFEdges; ++readEdges) {
+        in >> v1 >> v2;
+        edges.insert({v1, v2});
     }
 
-    return Graph(numberOfVertices, edges);
+    return Graph(numberOfVertices, std::move(edges));
 }
-
-
-
-
 
 void SumColouring::writeColouring(std::ostream &out, const std::vector<int> &colouring) {
     int currentVertex = 1;
